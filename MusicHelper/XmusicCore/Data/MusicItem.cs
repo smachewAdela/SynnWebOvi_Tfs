8fe3dee2095a9ddb26e6706bf64260c7330e0398
smachewAdela <samadela@gmail.com>
2020-06-18 21:18:04 +0300

    music helper in progress.

--- MusicHelper/XmusicCore/Data/MusicItem.cs
+++ MusicHelper/XmusicCore/Data/MusicItem.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 using HundredMilesSoftware.UltraID3Lib;
 using System.IO;
 using System.Data;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using TagLib;
 
 namespace XmusicCore
 {
     [Serializable]
     public class MusicItem : DbMusicItem
     {
+        public TagLib.File TagLibFile { get; }
+        public UltraID3 UltraID3Item { get; }
 
         public MusicItem()
         {
@@ -20,14 +25,68 @@ namespace XmusicCore
 
         public MusicItem(string filePath)
         {
-            var ii = TagLib.File.Create(filePath);
-            UltraID3 u = new UltraID3();
-            u.Read(filePath);
-            Init(ii, u);
+            try
+            {
+                TagLibFile = TagLib.File.Create(filePath);
+                UltraID3Item = new UltraID3();
+                UltraID3Item.Read(filePath);
+                Init(TagLibFile, UltraID3Item);
+            }
+            catch (Exception ex)
+            {
+            }
         }
         public MusicItem(IDataReader data)
         {
             Load(data);
+        }
+
+        public bool UltraID3HasData()
+        {
+            try
+            {
+                return UltraID3Item != null &&
+                          UltraID3Item.Album != null &&
+                          UltraID3Item.Artist != null &&
+                          UltraID3Item.Title != null &&
+                          UltraID3Item.Year?.ToString() != null &&
+                          UltraID3Item.TrackNum?.ToString() != null &&
+                          UltraID3Item.Genre != null;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool TagLibHasData()
+        {
+            try
+            {
+                return TagLibFile != null &&
+                          TagLibFile.Tag.Album != null &&
+                          TagLibFile.Tag.Artists != null &&
+                          TagLibFile.Tag.Title != null &&
+                          TagLibFile.Tag.Year.ToString() != null &&
+                          TagLibFile.Tag.Track.ToString() != null &&
+                          TagLibFile.Tag.Genres != null;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public string GetJson()
+        {
+            dynamic jText = new JObject();
+            jText.Album = UltraID3Item.Album;
+            jText.Artist = UltraID3Item.Artist;
+            jText.Title = UltraID3Item.Title;
+            jText.Year = UltraID3Item.Year?.ToString();
+            jText.TrackNum = UltraID3Item.TrackNum?.ToString();
+            jText.Genre = UltraID3Item.Genre;
+            return jText.ToString();
         }
 
         public MusicItem(TagLib.File f, UltraID3 u)
